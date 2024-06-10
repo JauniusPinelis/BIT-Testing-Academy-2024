188efb5e13485ee585a68c4fc5dfe6ef65d42102
JauniusPinelis <jauniuspinelis@gmail.com>
2024-06-10 18:05:36 +0300

    update

--- samples/registration-test/src/App.jsx
+++ samples/registration-test/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import RegistrationForm from './RegistrationForm';
+import ContactForm from './ContactForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <RegistrationForm />
+        <ContactForm />
       </header>
     </div>
   );
